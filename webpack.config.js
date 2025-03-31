388c030dabcbe19ea2028cbd39ba123d892e4fb8
dynamyc <balint.sebestyen2004@gmail.com>
2025-03-31 15:59:23 +0200

    build: remove comment used for debugging GH actions

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,6 @@ function getVersion() {
     return version;
   }
   let version = git.version();
-  console.log(git.branch())
   if(git.branch() !== "angular" && git.branch() !== "HEAD"){
     version += "-" + git.branch();
   }
